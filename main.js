dc95cf4c14cc29bf8d930924398c3a9aaa65d6c4
John Hass <john@blade.local.hasshouse.com>
2019-03-06 09:51:48 -0600

    Second Commit, Still doesn't do anything

---
+++ main.js
@@ -0,0 +1,29 @@
+const SerialPort = require('serialport')
+const { app, BrowserWindow } = require('electron')
+
+let win
+
+function createWindow () {
+  win = new BrowserWindow({ width: 800, height: 600, toolbar: false,  icon: __dirname + '/images/favicon.png', })
+  win.loadFile('index.html')
+  //win.webContents.openDevTools()
+  win.setMenuBarVisibility(false)
+  console.log(SerialPort.list);
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow()
+  }
+})
+
